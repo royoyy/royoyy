0f19cd3ac928d693ef02d4e1e80841cd1b7c7cd4
royoyy <47627371+royoyy@users.noreply.github.com>
2021-03-10 23:32:13 +0800

    add a directory

---
+++ ch11/stack/stack_general.go
@@ -0,0 +1,33 @@
+package stack
+
+import "error"
+
+type Stack []interface{}
+
+func (st Stack) Len() int {
+	return len(st)
+}
+func (st Stack) Cap() int {
+	return cap(st)
+}
+func (st Stack) IsEmpty() bool {
+	return st.Len() == 0
+}
+func (st *Stack) Push(e interface{}) {
+	*st = append(*st, e)
+}
+func (st Stack) Top() (interface{}, error) {
+	if len(st) == 0 {
+		return nil, errors.New("stack is empty")
+	}
+	return st[len(st)-1], nil
+}
+func (st *Stack) Pop() (interface{}, error) {
+	stk := *st // dereference to a local variable stk
+	if len(stk) == 0 {
+		return nil, errors.New("stack is empty")
+	}
+	top := stk[len(stk)-1]
+	*stack = stk[:len(stk)-1] // shrink the stack
+	return top, nil
+}
